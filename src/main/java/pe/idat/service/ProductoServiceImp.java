482380657e571464b81bd468fc6a40803dae222f
marcorevilla <rodriigoreviilla@gmail.com>
2022-10-03 18:00:42 -0500

    conneccion a bd

--- src/main/java/pe/idat/service/ProductoServiceImp.java
+++ src/main/java/pe/idat/service/ProductoServiceImp.java
@@ -15,33 +15,33 @@ public class ProductoServiceImp implements ProductoService {
 	@Override
 	public void guardar(Producto producto) {
 		// TODO Auto-generated method stub
-		repositorio.guardar(producto);
+		repositorio.save(producto);
 
 	}
 
 	@Override
 	public void actualizar(Producto producto) {
 		// TODO Auto-generated method stub
-		repositorio.actualizar(producto);
+		repositorio.saveAndFlush(producto);
 
 	}
 
 	@Override
 	public void eliminar(Integer id) {
 		// TODO Auto-generated method stub
-		repositorio.eliminar(id);
+		repositorio.deleteById(id);
 	}
 
 	@Override
 	public List<Producto> listar() {
 		// TODO Auto-generated method stub
-		return repositorio.listar();
+		return repositorio.findAll();
 	}
 
 	@Override
 	public Producto obtener(Integer id) {
 		// TODO Auto-generated method stub
-		return repositorio.obtener(id);
+		return repositorio.findById(id).orElse(null);
 	}
 
 }
