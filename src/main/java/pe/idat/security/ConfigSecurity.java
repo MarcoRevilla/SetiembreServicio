93033c799753a618c9b1399a9bd837a846045044
marcorevilla <rodriigoreviilla@gmail.com>
2022-10-25 13:02:46 -0500

    cambios con jwt

--- src/main/java/pe/idat/security/ConfigSecurity.java
+++ src/main/java/pe/idat/security/ConfigSecurity.java
@@ -1,8 +1,10 @@
 package pe.idat.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,9 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 //import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 
 
@@ -22,21 +26,55 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserDetailService service;
+	@Autowired
+	private TokenFilter filter;
+	@Autowired
+	private EntryPoint entrypoint;
+	
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("profesor").password(encriptado().encode("123")).roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("estudiante").password(encriptado().encode("123")).roles("USER");
+//		auth.inMemoryAuthentication().withUser("profesor").password(encriptado().encode("123")).roles("ADMIN");
+//		auth.inMemoryAuthentication().withUser("estudiante").password(encriptado().encode("123")).roles("USER");
+		auth.userDetailsService(service).passwordEncoder(encriptado());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//		.antMatchers("/producto/v1/listar").permitAll()
+//		.antMatchers("/producto/v1/**").access("hasRole('ADMIN')")
+//		.and()
+//		.httpBasic()
+//		.and()
+//		.csrf().disable();
+		
 		http.authorizeRequests()
-		.antMatchers("/producto/v1/listar").permitAll()
-		.antMatchers("/producto/v1/**").access("hasRole('ADMIN')")
+		.antMatchers("/crearToken").permitAll()
+		.anyRequest()
+		.authenticated()
 		.and()
-		.httpBasic()
+		.exceptionHandling()
+		.authenticationEntryPoint(entrypoint)
 		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
 		.csrf().disable();
 	}
 	
